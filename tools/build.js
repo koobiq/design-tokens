d068eff48dcfa188f418e68349e7a18312265a6d
Nikita Guryev <nikgurv@gmail.com>
2024-05-08 13:51:12 +0300

    fix: build hybrid esm/cjs (#DS-2180)

--- tools/build.js
+++ tools/build.js
@@ -11,12 +11,17 @@ build([
     }
 ]);
 
-const { copySync, copyFileSync, writeFileSync, readFileSync } = require('fs-extra');
+const { copySync, copyFileSync, writeFileSync, readFileSync, mkdirSync, removeSync, renameSync, existsSync } = require('fs-extra');
 
 const prepareTokens = (package) => {
     copySync(`packages/${package}`, `dist/${package}`);
 
     copyFileSync('./LICENSE', `dist/${package}/LICENSE`);
+}
+
+const prepareDir = (dirPath) => {
+    if (existsSync(dirPath)) { removeSync(dirPath); }
+    mkdirSync(dirPath);
 }
 
 const updateVersion = (package, version) => {
@@ -26,6 +31,30 @@ const updateVersion = (package, version) => {
     writeFileSync(`dist/${package}/package.json`, JSON.stringify(packageContent, undefined, 2))
 }
 
+const updateTokensPackage = () => {
+    const packagePath = 'dist/design-tokens/package.json';
+    const packageContent = JSON.parse(readFileSync(packagePath, 'utf8'));
+    const exportsConfig = {
+        ...packageContent.exports,
+        './*': {
+            require: './web/cjs/*',
+            import: './web/esm/*',
+            default: './*'
+        },
+        '.': {
+            require: './web/cjs/index.cjs',
+            import: './web/esm/index.mjs',
+            default: './web/esm/index.mjs'
+        }
+    }
+
+    packageContent.types = "web/index.d.ts";
+    packageContent.module = "web/index.mjs";
+    packageContent.exports = exportsConfig;
+
+    writeFileSync(packagePath, JSON.stringify(packageContent, undefined, 2))
+}
+
 const currentVersion = JSON.parse(readFileSync('package.json', 'utf8')).version;
 
 prepareTokens('design-tokens');
@@ -33,3 +62,7 @@ prepareTokens('tokens-builder');
 
 updateVersion('tokens-builder', currentVersion);
 updateVersion('design-tokens', currentVersion);
+
+prepareDir('dist/design-tokens/web/cjs');
+renameSync('dist/design-tokens/web/index.js', 'dist/design-tokens/web/index.mjs');
+updateTokensPackage();
