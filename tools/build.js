bcf42903f8e9cce365c2bb5b6c87c4f299ab35bb
Nikita Guryev <nikgurv@gmail.com>
2024-05-15 12:54:43 +0300

    fix: build hybrid esm/cjs (#DS-2180) (#1)
    * fix: build hybrid esm/cjs (#DS-2180)
    
    * fix: build js related to separate folder

--- tools/build.js
+++ tools/build.js
@@ -11,7 +11,7 @@ build([
     }
 ]);
 
-const { copySync, copyFileSync, writeFileSync, readFileSync } = require('fs-extra');
+const { copySync, copyFileSync, writeFileSync, readFileSync, renameSync } = require('fs-extra');
 
 const prepareTokens = (package) => {
     copySync(`packages/${package}`, `dist/${package}`);
@@ -26,6 +26,30 @@ const updateVersion = (package, version) => {
     writeFileSync(`dist/${package}/package.json`, JSON.stringify(packageContent, undefined, 2))
 }
 
+const updateTokensPackage = () => {
+    const packagePath = 'dist/design-tokens/package.json';
+    const packageContent = JSON.parse(readFileSync(packagePath, 'utf8'));
+    const exportsConfig = {
+        ...packageContent.exports,
+        './*': {
+            require: './web/js/*',
+            import: './web/js/*',
+            default: './*'
+        },
+        '.': {
+            require: './web/js/index.cjs',
+            import: './web/js/index.mjs',
+            default: './web/js/index.mjs'
+        }
+    }
+
+    packageContent.types = "web/index.d.ts";
+    packageContent.module = "web/js/index.mjs";
+    packageContent.exports = exportsConfig;
+
+    writeFileSync(packagePath, JSON.stringify(packageContent, undefined, 2))
+}
+
 const currentVersion = JSON.parse(readFileSync('package.json', 'utf8')).version;
 
 prepareTokens('design-tokens');
@@ -33,3 +57,6 @@ prepareTokens('tokens-builder');
 
 updateVersion('tokens-builder', currentVersion);
 updateVersion('design-tokens', currentVersion);
+
+renameSync('dist/design-tokens/web/js/index.js', 'dist/design-tokens/web/js/index.mjs');
+updateTokensPackage();
