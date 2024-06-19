798d44e220bcad4cea4f5428c54f2f9be11b55b6
Vitaly Govor <vitaly.govor@icloud.com>
2024-06-19 15:46:20 +0300

    feat(fonts): updated font-sizes for our typographic tokens (#DS-2168)

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
         'type-enum': [
             2,
             'always',
-            ['feat', 'feature', 'fix', 'refactor', 'docs', 'build', 'test', 'ci', 'chore']
+            ['feat', 'feature', 'fix', 'refactor', 'docs', 'build', 'test', 'ci', 'chore', 'fonts']
         ]
     }
 };
