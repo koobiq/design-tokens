41a8c5d8307c85541b2e3b1e3defd001c1924e4f
Nikita Guryev <nikgurv@gmail.com>
2024-08-07 19:16:02 +0300

    feat: added font grouping and unwrap for output

--- packages/tokens-builder/formats/variables.js
+++ packages/tokens-builder/formats/variables.js
@@ -1,4 +1,5 @@
 const { formatHelpers } = require('style-dictionary');
+const transform = require('style-dictionary/lib/common/transforms');
 
 module.exports = (StyleDictionary) => {
     StyleDictionary.registerFormat({
@@ -6,6 +7,25 @@ module.exports = (StyleDictionary) => {
         formatter: function ({ dictionary, options = {}, file }) {
             const selector = options.selector ? options.selector : `:root`;
             const { outputReferences } = options;
+
+            dictionary.allTokens = dictionary.allTokens.flatMap((token) => {
+                // only for typography
+                const shouldUnwrap = typeof token.value === 'object' && typeof token.original.value === 'string';
+                if (shouldUnwrap) {
+                    return Object.keys(token.value).map((key) => {
+                        const subToken = token.value[key];
+                        const res = {
+                            ...subToken,
+                            filePath: token.filePath,
+                            path: [...token.path, key],
+                            name: `kbq-${ [...token.path, key].join('-')}`,
+                        }
+                        res.attributes = { ...transform['attribute/cti'].transformer(res), font: true };
+                        return res;
+                    });
+                }
+                return token;
+            });
 
             dictionary.allTokens.forEach((token) => {
                 token.name = token.name.replace(/(light|dark)-/, '');
