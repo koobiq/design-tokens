ba5823b6ee6c731ac72dbcb0e243ad4b12c27c08
Artem Belik <artyombelyk@gmail.com>
2025-05-21 12:14:05 +0300

    chore: added eslint types and updated commitlint config

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,14 +4,7 @@
  * @type {import('@commitlint/types').UserConfig}
  */
 const config = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-            2,
-            'always',
-            ['feat', 'feature', 'fix', 'refactor', 'docs', 'build', 'test', 'ci', 'chore', 'fonts', 'shadows']
-        ]
-    }
+    extends: ['@commitlint/config-conventional']
 };
 
 module.exports = config;
