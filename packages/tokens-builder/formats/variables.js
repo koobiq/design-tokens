65b23e7a8102f6dc69c07b75bf2b4fa2733c1b21
Nikita Guryev <nikgurv@gmail.com>
2025-12-15 10:56:53 +0300

    feat: split tokens into separate files (#DS-3117) (#113)

--- packages/tokens-builder/formats/variables.js
+++ packages/tokens-builder/formats/variables.js
@@ -10,6 +10,31 @@ module.exports = (StyleDictionary) => {
             // apply custom transformations for tokens
             dictionary.allProperties = dictionary.allTokens = dictionary.allTokens.flatMap((token) => {
                 if (typeof token.value === 'object' && token.type === 'font') {
+                    return unwrapObjectTransformer(token, { font: true });
+                }
+                return token;
+            });
+
+            dictionary.allTokens.forEach((token) => {
+                token.name = token.name.replace(/(light|dark)-/, '');
+            });
+
+            return (
+                formatHelpers.fileHeader({ file }) +
+                `${selector} {\n` +
+                formatHelpers.formattedVariables({ format: 'css', dictionary, outputReferences }) +
+                `\n}\n`
+            );
+        }
+    });
+
+    StyleDictionary.registerFormat({
+        name: 'kbq-css/palette',
+        formatter: function ({ dictionary, options = {}, file }) {
+            const { outputReferences, selector = ':root' } = options;
+
+            dictionary.allProperties = dictionary.allTokens = dictionary.allTokens.flatMap((token) => {
+                if (typeof token.value === 'object') {
                     return unwrapObjectTransformer(token);
                 }
                 return token;
@@ -17,6 +42,7 @@ module.exports = (StyleDictionary) => {
 
             dictionary.allTokens.forEach((token) => {
                 token.name = token.name.replace(/(light|dark)-/, '');
+                token.name = token.name.replace(/"/g, '');
             });
 
             return (
